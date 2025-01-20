4f24b3e54386a0830ec3f6a16f9c442cb65ddb8d
WooFoo <sujanstha3220@gmail.com>
2025-01-20 14:05:40 -0500

    feat: wrapped theme context throughout the application

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
+import { ThemeProvider } from "./ThemeContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
-		<Toaster position="bottom-right" />
+		<ThemeProvider>
+			<App />
+			<Toaster position="bottom-right" />
+		</ThemeProvider>
 	</StrictMode>
 );
