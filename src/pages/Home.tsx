0288ff7a1ae4f1de82d4d002b9e0612cc025ca93
WooFoo <sujanstha3220@gmail.com>
2025-04-09 09:38:30 -0400

    feat: removed team members from UI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,38 +1,36 @@
-import Navbar from '../components/Navbar';
-import Hero from '../components/Hero';
-import About from '../components/sections/About';
-import Services from '../components/sections/Services';
-import Industries from '../components/sections/Industries';
-import Experience from '../components/sections/Experience';
-import ProjectJourney from '../components/sections/ProjectJourney';
-import Portfolio from '../components/sections/Portfolio';
-import CompanyHighlights from '../components/sections/CompanyHighlights';
-import Team from '../components/sections/Team';
-import Contact from '../components/sections/Contact';
-import Footer from '../components/sections/Footer';
-import Introduction from '../components/sections/Introduction';
-import WelcomeMessage from '../components/sections/WelcomeMessage';
-import UniqueAspect from '../components/sections/UniqueAspect';
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import About from "../components/sections/About";
+import Services from "../components/sections/Services";
+import Industries from "../components/sections/Industries";
+import Experience from "../components/sections/Experience";
+import ProjectJourney from "../components/sections/ProjectJourney";
+import Portfolio from "../components/sections/Portfolio";
+import CompanyHighlights from "../components/sections/CompanyHighlights";
+import Contact from "../components/sections/Contact";
+import Footer from "../components/sections/Footer";
+import Introduction from "../components/sections/Introduction";
+import WelcomeMessage from "../components/sections/WelcomeMessage";
+import UniqueAspect from "../components/sections/UniqueAspect";
 
 export default function Home() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <Introduction/>
-      <WelcomeMessage/>
-      <About />
-      <Services />
-      <ProjectJourney />
-      <UniqueAspect/>
-      <Portfolio />
-      <Industries />
-      <Experience />
-      <Team />
-      <CompanyHighlights />
-      {/* <Testimonials /> */}
-      <Contact />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<Hero />
+			<Introduction />
+			<WelcomeMessage />
+			<About />
+			<Services />
+			<ProjectJourney />
+			<UniqueAspect />
+			<Portfolio />
+			<Industries />
+			<Experience />
+			<CompanyHighlights />
+			{/* <Testimonials /> */}
+			<Contact />
+			<Footer />
+		</>
+	);
 }
