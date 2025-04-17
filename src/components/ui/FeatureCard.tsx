db7906acfa42c0713241ccfb1553230b952139e8
WooFoo <sujanstha3220@gmail.com>
2025-04-17 13:43:08 -0400

    feat: added and changed some card components

--- src/components/ui/FeatureCard.tsx
+++ src/components/ui/FeatureCard.tsx
@@ -1,29 +1,46 @@
 import { LucideIcon } from "lucide-react";
 import { cn } from "../../utils";
+import { Card, CardHeader, CardTitle, CardDescription } from "./Cards";
 
 interface FeatureCardProps {
-  title: string;
-  description: string;
-  icon: LucideIcon | React.ElementType;
-  image?: boolean;
+	title: string;
+	description: string;
+	icon: LucideIcon | React.ElementType;
+	image?: boolean;
 }
 
 export default function FeatureCard({
-  title,
-  description,
-  icon: Icon,
-  image,
+	title,
+	description,
+	icon: Icon,
+	image,
 }: FeatureCardProps) {
-  return (
-    <div className="p-6 rounded-lg bg-green-1000 dark:bg-[#1E293B]  hover:dark:bg-dark-200 hover:opacity-90 transition-colors">
-      <div className={cn("w-12 h-12 rounded-lg  bg-gray-50/20 dark:bg-primary-500/10 flex items-center justify-center mb-4",image&& 'bg-[#ebeced]')}>
-        {!image && (
-          <Icon className="h-6 w-6 text-gray-50 dark:text-primary-1000" />
-        )}
-        {image && (Icon as React.ElementType)}
-      </div>
-      <h3 className="text-lg font-semibold text-white mb-2">{title}</h3>
-      <p className="text-gray-200 dark:text-gray-400">{description}</p>
-    </div>
-  );
+	return (
+		<Card className="bg-white dark:bg-[#1E293B] text-gray-900 dark:text-white hover:opacity-95 transition-all">
+			<CardHeader>
+				<div
+					className={cn(
+						"w-12 h-12 rounded-lg flex items-center justify-center mb-3",
+						image
+							? "bg-[#ebeced]"
+							: "bg-gray-100 dark:bg-primary-500/10"
+					)}
+				>
+					{image ? (
+						<Icon />
+					) : (
+						<Icon className="h-6 w-6 text-primary dark:text-primary-400" />
+					)}
+				</div>
+
+				<CardTitle className="text-lg font-semibold text-gray-700 dark:text-white">
+					{title}
+				</CardTitle>
+
+				<CardDescription className="text-sm text-gray-500 dark:text-gray-400">
+					{description}
+				</CardDescription>
+			</CardHeader>
+		</Card>
+	);
 }
