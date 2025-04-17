a31a34c75f3d583d61078e546cee4b9bf33d0f1a
WooFoo <sujanstha3220@gmail.com>
2025-04-16 22:25:44 -0400

    chore :industry card changes

--- src/components/ui/IndustryCard.tsx
+++ src/components/ui/IndustryCard.tsx
@@ -1,23 +1,30 @@
 import { LucideIcon } from "lucide-react";
+import { Card, CardHeader, CardTitle, CardDescription } from "./Cards";
 
 interface IndustryCardProps {
-  icon: LucideIcon;
-  title: string;
-  description: string;
+	icon: LucideIcon;
+	title: string;
+	description: string;
 }
 
 export default function IndustryCard({
-  icon: Icon,
-  title,
-  description,
+	icon: Icon,
+	title,
+	description,
 }: IndustryCardProps) {
-  return (
-    <div className="p-6 rounded-lg bg-green-1000 dark:bg-[#1E293B]  hover:dark:bg-dark-200 hover:opacity-90 overflow-hidden">
-      <div className="w-12 h-12 rounded-lg  bg-gray-50/20 dark:bg-primary-500/10 flex items-center justify-center mb-4">
-        <Icon className="h-6 w-6 text-gray-50 dark:text-primary-1000" />
-      </div>
-      <h3 className="text-lg font-semibold text-white mb-2">{title}</h3>
-      <p className="text-gray-200 dark:text-gray-400">{description}</p>
-    </div>
-  );
+	return (
+		<Card className="bg-white dark:bg-[#1E293B] text-gray-900 dark:text-white hover:opacity-95 transition-all">
+			<CardHeader>
+				<div className="w-12 h-12 rounded-lg bg-gray-100 dark:bg-primary-500/10 flex items-center justify-center mb-2">
+					<Icon className="h-6 w-6 text-primary dark:text-primary-400" />
+				</div>
+				<CardTitle className="text-primary-1000 dark:text-white text-lg font-semibold">
+					{title}
+				</CardTitle>
+				<CardDescription className="text-sm text-gray-500 dark:text-gray-400">
+					{description}
+				</CardDescription>
+			</CardHeader>
+		</Card>
+	);
 }
